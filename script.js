7b19f266b26195907caad3beb8a938a3ca4ae163
romeopapa1992 <rafal.proski.programista@gmail.com>
2024-04-11 22:37:39 +0200

    version withouth css

--- script.js
+++ script.js
@@ -14,41 +14,41 @@
     
     // Draw the chart and set the chart values
     function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-      ['Task', 'Hours per Day'],
-      ['Work', 8],
-      ['Friends', 2],
-      ['Eat', 2],
-      ['TV', 2],
-      ['Gym', 2],
-      ['Sleep', 8]
-    ]);
-    
-      // Optional; add a title and set the width and height of the chart
-      var options = {'width':550, 'height':400};
-    
-      // Display the chart inside the <div> element with id="piechart"
-      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-      chart.draw(data, options);
-    }
-
-
-    google.charts.load('current', {'packages':['table']});
-      google.charts.setOnLoadCallback(drawTable);
-
-      function drawTable() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Name');
-        data.addColumn('number', 'Salary');
-        data.addColumn('boolean', 'Full Time Employee');
-        data.addRows([
-          ['Mike',  {v: 10000, f: '$10,000'}, true],
-          ['Jim',   {v:8000,   f: '$8,000'},  false],
-          ['Alice', {v: 12500, f: '$12,500'}, true],
-          ['Bob',   {v: 7000,  f: '$7,000'},  true]
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Amount'],
+          ['Food', 100],
+          ['Transport', 50],
+          ['House', 300],
+          ['Shopping', 200],
+          ['Entertaiment', 80],
         ]);
-
-        var table = new google.visualization.Table(document.getElementById('table_div'));
-
-        table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+      
+        // Optional; add a title and set the width and height of the chart
+        var options = {'width':550, 'height':400};
+      
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
       }
+
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Amount (PLN)');
+    data.addColumn('string', 'Category');
+    data.addColumn('string', 'Comment');
+    data.addRows([
+        ['2024-03-01', 100, 'Food', 'Lunch with friends'],
+        ['2024-03-04', 50, 'Transport', 'Bus ticket'],
+        ['2024-03-11', 300, 'House', 'Groceries'],
+        ['2024-03-18', 200, 'Shopping', 'New shoes'],
+        ['2024-03-28', 80, 'Entertainment', 'Theatre']
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+}
